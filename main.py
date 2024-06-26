c7e5df667fac4c81cf94ea68476028621e02fc35
jesus <jesus_estrada5@my.vcccd.edu>
2024-06-26 15:20:20 -0700

    CS V11

--- main.py
+++ main.py
@@ -9,16 +9,22 @@ def main():
     ########################################
     Code Your Program here
     """
+    # print statement, number1, number2, number3
     print(f'{number1} {number2} {number3}')
     
+    # determines whether number1 is less than number2 and number3, outputs number 1 if less 
     if number1 < number2 and number1 < number3:
         min_value = number1
-        
+    
+    # determines wheter number2 is less than 3, outputs number 2 if less
     elif number2 < number3:
         min_value = number2
+    
+    # determines wheter number 3 is less than number1 and number2 in previous if and elif statements
     else:
         min_value = number3
     
+    # print 'smallest number statement' and the smallest number produced by the random generator
     print(f'smallest number : {min_value}')
     """"
     ########################################
